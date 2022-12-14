929e03952ff207b9c7e9290bbe34dfc8e1b017fb
Glazyrin, Leonid <glazyrin.leonid@courrier.uqam.ca>
2022-12-14 16:45:28 -0500

    Add tests for all remaining functions in main.c

--- tests.c
+++ tests.c
@@ -1,5 +1,10 @@
 #include "tests.h"
 
+/**
+ * -----------------------
+ * Unit tests for main.c
+ * -----------------------
+ */
 void test_open_file() {
     // Try opening a file that exists
     FILE* fptr = open_file("test/banque_vide.txt", "r");
@@ -14,48 +19,17 @@ void test_close_file() {
 }
 
 void test_check_args() {
-    // // Test with invalid number of arguments.
-    int argc = 1;
-    // char* argv[] = { "./recherche" };
-    // CU_ASSERT_FATAL(check_args(argc, argv));
-
     // Test with valid number of arguments and valid recipe file.
-    argc = 2;
+    int argc = 2;
     char* argv2[] = { "./recherche", "test/banque1.txt" };
     check_args(argc, argv2);
     CU_ASSERT_EQUAL(1, 1);
-
-    // // Test with valid number of arguments and invalid recipe file.
-    // argc = 2;
-    // char* argv[] = { "./recherche", "badfile.txt" };
-    // CU_ASSERT_DEATH(check_args(argc, argv), "Erreur dans l'ouverture du fichier");
 
     // Test with valid number of arguments and valid statistics arguments.
     argc = 4;
     char* argv4[] = { "./recherche", "test/banque1.txt" , "-S", "stats.txt" };
     check_args(argc, argv4);
     CU_ASSERT_EQUAL(1, 1);
-
-    // // Test with valid number of arguments and invalid statistics arguments.
-    // argc = 4;
-    // char* argv[] = { "./recherche", "-S", "badfile.txt" };
-    // CU_ASSERT_DEATH(check_args(argc, argv), "Erreur dans l'ouverture du fichier");
-
-    // Test with valid number of arguments and invalid statistics flag.
-    // argc = 4;
-    // char* argv[] = { "./recherche", "test/banque1.txt", "-B", "stats.txt" };
-    // CU_ASSERT_FATAL(check_args(argc, argv));
-}
-
-
-void test_check_stats_args() {
-    // TODO: Add test cases for the check_stats_args function.
-}
-
-void test_throw_error_args_count() {
-    // CU_ASSERT_DEATH(throw_error_args_count(), "nombre arguments invalide");
-    // compile your code with the -DCU_ENABLE_EXPERIMENTAL_C_ASSERT flag and link it with the -lcunit -lcunitx libraries. 
-    // CU_ASSERT_FATAL(throw_error_args_count());
 }
 
 void test_load_recipes(void) {
@@ -87,17 +61,84 @@ void test_load_recipes(void) {
     CU_ASSERT_STRING_EQUAL(category->recipes->name, "recipe2");
 }
 
-void test_load_one_recipe() {
-    // TODO: Add test cases for the load_one_recipe function.
+void test_load_one_recipe(void) {
+    recipes_book *book;
+    recipes_book_initialize(book);
+
+    char buffer[MAX_LINE_LENGHT] = "Banana Bread [Breakfast] [Dessert]";
+
+    // Test adding a recipe to the book
+    load_one_recipe(buffer, book);
+    CU_ASSERT_EQUAL(book->num_categories, 2);
+
+    struct category_node *current = book->first;
+    CU_ASSERT_EQUAL(strcmp(current->category, "Breakfast"), 0);
+    CU_ASSERT_EQUAL(current->num_recipes, 1);
+    CU_ASSERT_EQUAL(strcmp(current->recipes->name, "Banana Bread"), 0);
+
+    current = current->next;
+    CU_ASSERT_EQUAL(strcmp(current->category, "Dessert"), 0);
+    CU_ASSERT_EQUAL(current->num_recipes, 1);
+    CU_ASSERT_EQUAL(strcmp(current->recipes->name, "Banana Bread"), 0);
 }
 
-void test_load_recipe_into_categories() {
-    // TODO: Add test cases for the load_recipe_into_categories function.
+void test_load_recipe_into_categories(void) {
+    // Create a test recipe book
+    recipes_book *book;
+    recipes_book_initialize(book);
+
+    // Create a test recipe string with two categories
+    char buffer[MAX_LINE_LENGHT] = "Banana Bread [Breakfast] [Dessert]";
+
+    // Test adding a recipe to the book
+    load_recipe_into_categories("Banana Bread", buffer, book);
+
+    // Check that the book has two categories
+    CU_ASSERT_EQUAL(book->num_categories, 2);
+
+    // Check that the first category has the correct name and contains the recipe
+    struct category_node *current = book->first;
+    CU_ASSERT_EQUAL(strcmp(current->category, "Breakfast"), 0);
+    CU_ASSERT_EQUAL(current->num_recipes, 1);
+    CU_ASSERT_EQUAL(strcmp(current->recipes->name, "Banana Bread"), 0);
+
+    // Check that the second category has the correct name and contains the recipe
+    current = current->next;
+    CU_ASSERT_EQUAL(strcmp(current->category, "Dessert"), 0);
+    CU_ASSERT_EQUAL(current->num_recipes, 1);
+    CU_ASSERT_EQUAL(strcmp(current->recipes->name, "Banana Bread"), 0);
+
+    // Check that the function correctly handles an empty recipe string
+    strcpy(buffer,"");
+
+    // Test adding a recipe to the book
+    load_recipe_into_categories("Banana Bread", buffer, book);
+
+    // Check that the book still has only 2 categories
+    CU_ASSERT_EQUAL(book->num_categories, 2);
 }
 
-void test_load_recipe_into_one_category() {
-    // TODO: Add test cases for the load_recipe_into_one_category function.
+void test_load_recipe_into_one_category(void) {
+    // Create a test recipe book
+    recipes_book *book;
+    recipes_book_initialize(book);
+
+    // Create a test recipe string with one category
+    char buffer[MAX_LINE_LENGHT] = "Banana Bread [Breakfast]";
+
+    // Test adding a recipe to the book
+    load_recipe_into_one_category(buffer, book, "Banana Bread", 13, 23);
+
+    // Check that the book has one category
+    CU_ASSERT_EQUAL(book->num_categories, 1);
+
+    // Check that the category has the correct name and contains the recipe
+    struct category_node *current = book->first;
+    CU_ASSERT_EQUAL(strcmp(current->category, "Breakfast"), 0);
+    CU_ASSERT_EQUAL(current->num_recipes, 1);
+    CU_ASSERT_EQUAL(strcmp(current->recipes->name, "Banana Bread"), 0);
 }
+
 
 void test_get_recipe_name() {
     // First
@@ -105,8 +146,8 @@ void test_get_recipe_name() {
     char expected_name[MAX_LINE_LENGHT] = "Lasagne";
     char result_name[MAX_LINE_LENGHT];
     result_name[0] = '\0';
-    get_recipe_name(result_name, buffer);
 
+    get_recipe_name(result_name, buffer);
     CU_ASSERT_STRING_EQUAL(expected_name, result_name);
 
     // Second
@@ -115,13 +156,19 @@ void test_get_recipe_name() {
     result_name[0] = '\0';
 
     get_recipe_name(result_name, buffer);
-
     CU_ASSERT_STRING_EQUAL(expected_name, result_name);
 }
 
 void test_run_prompt() {
     // TODO: Add test cases for the run_prompt function.
 }
+
+/**
+ * -----------------------
+ * Unit tests for linkedList.c
+ * -----------------------
+ */
+
 
 int main() {
     // Initialize the CUnit test registry.
@@ -142,8 +189,10 @@ int main() {
         (NULL == CU_add_test(suite, "test_close_file", test_close_file)) ||
         (NULL == CU_add_test(suite, "test_check_args", test_check_args)) ||
         (NULL == CU_add_test(suite, "test_load_recipes", test_load_recipes)) ||
+        (NULL == CU_add_test(suite, "test_load_one_recipe", test_load_one_recipe)) ||
+        (NULL == CU_add_test(suite, "test_load_recipe_into_categories", test_load_recipe_into_categories)) ||
+        (NULL == CU_add_test(suite, "test_load_recipe_into_one_category", test_load_recipe_into_one_category)) ||
         (NULL == CU_add_test(suite, "test_get_recipe_name", test_get_recipe_name))
-        // (NULL == CU_add_test(suite, "test_close_file", test_close_file))
     ) {
         CU_cleanup_registry();
         return CU_get_error();
--- tests.h
+++ tests.h
@@ -8,3 +8,8 @@
 void test_open_file(void);
 void test_close_file(void);
 void test_check_args(void);
+void test_load_recipes(void);
+void test_load_one_recipe(void);
+void test_load_recipe_into_categories(void);
+void test_load_recipe_into_one_category(void);
+void test_get_recipe_name(void);
