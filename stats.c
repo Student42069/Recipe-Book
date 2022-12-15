207289f8afd1e86959d54320d0dd809d994ee0f2
Glazyrin, Leonid <glazyrin.leonid@courrier.uqam.ca>
2022-12-15 14:46:20 -0500

    Start refactoring stats

--- stats.c
+++ stats.c
@@ -3,16 +3,15 @@
 
 void produce_stats(recipes_book *book, char *file_name, char *recipes_file_name) {
     FILE *file = open_file(file_name, "w");
-
     fprintf(file, "Le nombre de lignes dans le fichier d'entrée: %d\n", num_lines(recipes_file_name));
     fprintf(file, "Le nombre de mots sans doublons: %d\n", num_unique_words(recipes_file_name));
     fprintf(file, "Le nombre de mots avec doublons: %d\n", num_words(recipes_file_name));
     fprintf(file, "La lettre la plus fréquente (sans considérer les doublons): %c\n", most_frequent_letter(recipes_file_name));
     fprintf(file, "Le nombre de catégories: %d\n", num_categories(book));
     fprintf(file, "Le nombre de recettes: %d\n", num_lines(recipes_file_name));
+    fprintf(file, "La catégorie la plus populaire (la plus fréquente): %s\n", category_with_most_recipes(book));
     fprintf(file, "La catégorie qui a le plus grand nombre de recettes: %s\n", category_with_most_recipes(book));
     fprintf(file, "La recette la plus longue (en termes de nombre de caractères): %s\n", longest_recipe(book));
-
     close_file(file);
 }
 
@@ -33,10 +32,8 @@ int num_unique_words(char *file_name) {
     char *words[num_words(file_name)];
     int count = 0;
     char buffer[MAX_LINE_LENGHT];
-
     while (fgets(buffer, MAX_LINE_LENGHT, file) != NULL) {
         char *word = strtok(buffer, " ");
-
         while (word != NULL) {
             int found = 0;
             for (int i = 0; i < count; i++)
@@ -44,16 +41,12 @@ int num_unique_words(char *file_name) {
                     found = 1;
                     break;
                 }
-
             if (!found) 
                 words[count++] = word;
-
             word = strtok(NULL, " ");
         }
     }
-
     close_file(file);
-
     return count;
 }
 
@@ -61,12 +54,11 @@ int num_words(char *recipes_file_name) {
     FILE *file = open_file(recipes_file_name, "r");
     int count = 0;
     char buffer[MAX_LINE_LENGHT];
-
     while (fgets(buffer, MAX_LINE_LENGHT, file) != NULL) {
         char *word = strtok(buffer, " ");
         while (word != NULL) {
-        count++;
-        word = strtok(NULL, " ");
+            count++;
+            word = strtok(NULL, " ");
         }
     }
     close_file(file);
@@ -100,26 +92,20 @@ char most_frequent_letter(char *file_name) {
 
     close_file(file);
 
-    unsigned int i;
-    unsigned int j;
+    unsigned int i, j;
     int counts[26] = {0};
-    for (i = 0; i < count; ++i) {
-        for (j = 0; j < (unsigned int) strlen(words[i]); ++j) {
-            if (isalpha(words[i][j])) {
+    for (i = 0; i < count; ++i)
+        for (j = 0; j < (unsigned int) strlen(words[i]); ++j)
+            if (isalpha(words[i][j]))
                 counts[tolower(words[i][j]) - 'a']++;
-            }
-        }
-    }
 
     int max = 0;
     char letter = 'a';
-    for (int i = 0; i < 26; i++) {
+    for (int i = 0; i < 26; i++)
         if (counts[i] > max) {
             max = counts[i];
             letter = 'a' + i;
         }
-    }
-
     return letter;
 }
 
@@ -134,8 +120,8 @@ char *category_with_most_recipes(recipes_book *book) {
 
     while (current_category != NULL) {
         if (current_category->num_recipes > (unsigned int) max_num_recipes) {
-        category_with_most_recipes = current_category->category;
-        max_num_recipes = current_category->num_recipes;
+            category_with_most_recipes = current_category->category;
+            max_num_recipes = current_category->num_recipes;
         }
         current_category = current_category->next;
     }
@@ -161,3 +147,38 @@ char *longest_recipe(recipes_book *book) {
     }
     return longest_recipe;
 }
+
+
+// void iterate_on_recipes(char **longest_recipe, unsigned int max_recipe_length, struct recipe_node *current_recipe) {
+    
+//     while (current_recipe != NULL) {
+//         if ((unsigned int) strlen(current_recipe->name) > max_recipe_length) {
+//             longest_recipe = &current_recipe->name;
+//             max_recipe_length = strlen(current_recipe->name);
+//         }
+//         current_recipe = current_recipe->next;
+//     }
+
+// }
+
+// void iterate_on_categories(char **longest_recipe, unsigned int max_recipe_length, struct category_node *current_category) {
+    
+//     while (current_category != NULL) {
+//         struct recipe_node *current_recipe = current_category->recipes;
+//         iterate_on_recipes(longest_recipe, max_recipe_length, current_recipe);
+//         current_category = current_category->next;
+//     }
+// }
+
+
+
+// char *longest_recipe(recipes_book *book) {
+//     char *longest_recipe = NULL;
+//     unsigned int max_recipe_length = 0;
+//     struct category_node *current_category = book->first;
+
+//     iterate_on_categories(&longest_recipe, max_recipe_length, current_category);
+    
+//     return longest_recipe;
+// }
+
